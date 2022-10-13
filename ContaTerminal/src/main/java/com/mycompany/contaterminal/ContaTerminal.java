c6c28287ac67571130bb7cd0a80b3c80e1d8551d
Darlan de Oliveira Menezes <darlanoliv1@gmail.com>
2022-10-13 10:52:14 -0400

    envio definitivo 0.1

--- ContaTerminal/src/main/java/com/mycompany/contaterminal/ContaTerminal.java
+++ ContaTerminal/src/main/java/com/mycompany/contaterminal/ContaTerminal.java
@@ -1,11 +1,31 @@
 
 package com.mycompany.contaterminal;
 
+import java.util.Scanner;
+
 
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("1, 2 trestando...");
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        Float saldo;
+         
+        Scanner entradaDados = new Scanner(System.in);
+          System.out.printf("Por favor, digite o número da Conta !\n");
+        numeroConta = entradaDados.nextInt();
+          System.out.printf("Por favor, digite o número da Agencia !\n");
+        agencia = entradaDados.next();
+          System.out.printf("Por favor, digite o nome do titular da Conta !\n");
+        nomeCliente = entradaDados.next();
+          System.out.printf("Por favor, digite um valor para Deposito !\n");
+        saldo = entradaDados.nextFloat();
+        
+        System.out.println("Olá" + " " + nomeCliente);
+        System.out.println("obrigado por criar uma conta em nosso banco.");
+        System.out.println("sua agência é:" + " " + agencia + " " + "conta:" + " " + numeroConta);
+        System.out.printf("e seu saldo:%.2f\n", saldo);
+        System.out.println("já está disponível para saque.");
     }
 }
